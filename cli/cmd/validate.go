436c745df0e10bd14afbf99b3a2385024a7e0398
Šimon Lukašík <isimluk@fedoraproject.org>
2020-01-17 10:47:35 +0100

    Improve `oscalkit validate` to automatically select relevant OSCAL schema
    This will highly improve usability of the tool and thus increase chances that
    people will validate their own files.
    
    Further, validating file automatically shall turn out beneficial to the tool
    itself, once we start validating file oscalkit itself produces.

--- cli/cmd/validate.go
+++ cli/cmd/validate.go
@@ -1,10 +1,8 @@
 package cmd
 
 import (
-	"path/filepath"
-
-	"github.com/docker/oscalkit/validator"
-	"github.com/sirupsen/logrus"
+	"fmt"
+	"github.com/docker/oscalkit/pkg/oscal_source"
 	"github.com/urfave/cli"
 )
 
@@ -12,49 +10,29 @@ var schemaFile string
 
 // Validate ...
 var Validate = cli.Command{
-	Name:  "validate",
-	Usage: "validate files against OSCAL XML and JSON schemas",
-	Description: `Validate OSCAL-formatted XML files against a specific XML schema (.xsd)
-	 or OSCAL-formatted JSON files against a specific JSON schema`,
-	ArgsUsage: "[files...]",
-	Flags: []cli.Flag{
-		cli.StringFlag{
-			Name:        "schema, s",
-			Usage:       "schema file to validate against",
-			Destination: &schemaFile,
-		},
-	},
+	Name:        "validate",
+	Usage:       "validate files against OSCAL XML and JSON schemas",
+	Description: `Validate OSCAL-formatted files against a specific OSCAL schema`,
+	ArgsUsage:   "[files...]",
 	Before: func(c *cli.Context) error {
 		if c.NArg() < 1 {
 			return cli.NewExitError("oscalkit validate requires at least one argument", 1)
 		}
-
-		if schemaFile == "" {
-			return cli.NewExitError("missing schema file (-s) flag", 1)
-		}
-
-		for _, f := range c.Args() {
-			if filepath.Ext(f) == ".xml" && filepath.Ext(schemaFile) != ".xsd" {
-				return cli.NewExitError("Schema file should be .xsd", 1)
-			}
-
-			if filepath.Ext(f) == ".json" && filepath.Ext(schemaFile) != ".json" {
-				return cli.NewExitError("Schema file should be .json", 1)
-			}
-		}
-
 		return nil
 	},
 	Action: func(c *cli.Context) error {
-		schemaValidator := validator.New(schemaFile)
+		for _, filePath := range c.Args() {
+			os, err := oscal_source.Open(filePath)
+			if err != nil {
+				return cli.NewExitError(fmt.Sprintf("Could not open oscal file: %v", err), 1)
+			}
+			defer os.Close()
 
-		if err := schemaValidator.Validate(c.Args()...); err != nil {
-			logrus.Error(err)
-			return nil
+			err = os.Validate()
+			if err != nil {
+				return cli.NewExitError(err, 1)
+			}
 		}
-
-		logrus.Debug("Validation complete")
-
 		return nil
 	},
 }
